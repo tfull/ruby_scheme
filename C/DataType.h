26f1316aef3ce597781cbb6177b6ad6e8f8ae27b
tfull <tfull154008@gmail.com>
2016-03-27 17:59:49 +0900

    file

---
+++ C/DataType.h
@@ -0,0 +1,7 @@
+#ifndef __DATA_TYPE_H__
+#define __DATA_TYPE_H__
+
+typedef struct{
+}Token;
+
+#endif
---
+++ C/Main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    read();
+    display();
+    
+    return 0;
+}
