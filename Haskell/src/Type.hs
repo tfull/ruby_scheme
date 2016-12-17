061f423d53827cb472c8cd87fa7cfd01598a35d9
tfull <tfull154008@gmail.com>
2016-12-17 18:10:52 +0900

    type

--- Haskell/src/Type.hs
+++ Haskell/src/Type.hs
@@ -1,6 +1,15 @@
 module Type where
 
-data Value = VInt Int
+import qualified Data.Ratio
+import qualified Data.Complex
+
+data SchemeNumber = SchemeInt Integer
+    | SchemeFloat Double
+    | SchemeRational Data.Ratio.Rational Integer
+    | SchemeComplex Data.Complex.Complex Double
+    deriving Show
+
+data Value = VNumber SchemeNumber
     | VNil
     | VSyntax String
     | VSubroutine String
@@ -27,3 +36,10 @@ valueToString (VCons v0 v1) = "(" ++ sub v0 v1 ++ ")"
         sub v0 VNil = valueToString v0
         sub v0 (VCons v1 v2) = valueToString v0 ++ " " ++ sub v1 v2
         sub v0 v1 = valueToString v0 ++ " . " ++ valueToString v1
+
+instance Num SchemeNumber where
+    (SchemeInt x0) + (SchemeInt x1) = SchemeInt $ x0 + x1
+    (SchemeFloat x) + (SchemeFloat y) = SchemeFloat $ x + y
+    (SchemeRational x0) + (SchemeRational x1) = SchemeRational $ x0 + x1
+    (SchemeComplex x0) + (SchemeComplex x0) = SchemeComplex $ x0 + x1
+    (SchemeInt x0) - (SchemeInt x1) = SchemeInt $ x0 - x1
